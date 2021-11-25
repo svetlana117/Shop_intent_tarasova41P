81142a397e8ea135c7e6e12f13a008bf6aa2a883
svetlana.28.02 <svetlana.28.02@mail.ru>
2021-11-25 20:28:05 +0300

    авторизация

--- app/src/main/java/com/example/sr_tarasova/DBHelper.java
+++ app/src/main/java/com/example/sr_tarasova/DBHelper.java
@@ -11,11 +11,14 @@ public class DBHelper extends SQLiteOpenHelper {
     public static final int DATABASE_VERSION = 1;
     public static final String DATABASE_NAME = "contactDb";
     public static final String TABLE_CONTACTS = "contacts";
+    public static final String TABLE_USERS= "users";
 
 
     public static final String KEY_ID = "_id";
     public static final String KEY_TOVAR = "product";
     public static final String KEY_PRICE = "price";
+    public static final String KEY_LOGIN = "login";
+    public static final String KEY_PASSWORD = "password";
 
     public DBHelper(@Nullable Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -25,12 +28,13 @@ public class DBHelper extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase db) {
         db.execSQL("create table " + TABLE_CONTACTS + "(" + KEY_ID
                 + " integer primary key," + KEY_TOVAR + " text," + KEY_PRICE + " text" + ")");
+        db.execSQL("create table " + TABLE_USERS + "(" + KEY_ID  + " integer primary key," + KEY_LOGIN + " text," + KEY_PASSWORD + " text" +")");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("drop table if exists " + TABLE_CONTACTS);
-
+        db.execSQL("drop table if exists " + TABLE_USERS);
         onCreate(db);
 
     }
